7e640b635b054590b0b7e1a8b57f19e293e52a92
ftejeria <tejeria.felipe@gmail.com>
2022-08-18 22:08:27 -0300

    Delete book by api

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { Book } from '../models/Book.js'
+import mongoose from 'mongoose'
 
 const router = express.Router()
 
-router.get('/get-book', (req, res) => {})
+router.get('/get-books', async (req, res) => {
+  const books = await Book.find()
+  res.send(books)
+})
+
 router.post('/add-book', (req, res) => {
   const name = req.body.name
   const author = req.body.author
@@ -17,7 +22,17 @@ router.post('/add-book', (req, res) => {
   book.save()
   res.send(book)
 })
+
 router.put('', (req, res) => {})
-router.delete('/remove-book', (req, res) => {})
+
+router.delete('/remove-book', async (req, res) => {
+  const bookId = req.query.bookId
+  if (mongoose.Types.ObjectId.isValid(bookId)) {
+    const book = await Book.findByIdAndDelete(bookId)
+    return book ? res.send(`Book with id ${bookId} deleted`) : res.send('Book not found')
+  } else {
+    return res.send('Invalid book id')
+  }
+})
 
 export { router as bookRouter }
